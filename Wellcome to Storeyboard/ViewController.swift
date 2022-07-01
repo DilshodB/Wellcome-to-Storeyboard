6d056ab31982068bb56b22040164fc2ca0df06cf
Dilshod <nepkeid@mail.ru>
2022-07-01 11:12:11 +0500

    Initial Commit

---
+++ Wellcome to Storeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wellcome to Storeyboard
+//
+//  Created by 1 1 on 01/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wellcome to Storeyboard/WelcomeToStoryboard.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeToStoryboard.swift
+//  Wellcome to Storeyboard
+//
+//  Created by 1 1 on 01/07/22.
+//
+
+import UIKit
+
+class WelcomeToStoryboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
